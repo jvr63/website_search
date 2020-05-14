dc03c71e8eb6a12634b54d42c6f6d06708e49945
johnny <vmjuan90@gmail.com>
2020-05-14 12:27:53 +0200

    update

--- controllers/main.py
+++ controllers/main.py
@@ -43,16 +43,3 @@ class WebsiteSaleGuadalstoreSearch(WebsiteSale):
                 domain += [('attribute_line_ids.value_ids', 'in', ids)]
 
         return domain
-
-    @http.route()
-    def get_combination_info_website(self, product_template_id, product_id, combination, add_qty, **kw):
-        """
-            Added ref and barcode to original response
-        """
-        res = super(WebsiteSaleGuadalstore, self).get_combination_info_website(product_template_id, product_id, combination, add_qty, **kw)
-        product = request.env['product.product'].browse(int(product_id))
-        res.update({
-            'barcode': product.barcode,
-            'default_code': product.default_code
-             })
-        return res
