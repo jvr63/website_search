8156a1f545605c3238637e6fefa95372d4e2c995
johnny <vmjuan90@gmail.com>
2020-08-24 22:06:41 +0200

    WIP

--- __manifest__.py
+++ __manifest__.py
@@ -1,11 +1,11 @@
 {
     'name': "Modify website search",
     'summary': 'Modify website search',
-    'description': "Añadir busqueda en WEB por Código de Barras",
-    'author': 'Juan Vázquez Moreno y JVR',
+    'description': "Mejora la búsquedas de productos",
+    'author': 'Juan Vázquez Moreno <vmjuna90@gmail.com>;JVR <jvr@guadalsistema.com>',
     'category': 'Website',
     'version': '1.0',
-    'depends': ['website_sale'],
+    'depends': ['website_sale', 'product_brand'],
     'data': [
         'views/website_search.xml',
     ],
