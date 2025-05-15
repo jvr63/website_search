8104df1c50cf8bada0ca717a485a5c2c07a1d679
jvr63 <48151336+jvr63@users.noreply.github.com>
2025-05-15 19:04:45 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
     'description': "Mejora la búsquedas de productos en shop añadiendo busqueda por codigo de barras",
     'author': 'Juan Vázquez Moreno <vmjuna90@gmail.com>;JVR <jvr@guadalsistema.com>',
     'category': 'Website',
-    'version': '17.0.0',
+    'version': '17.0.0.0',
     'depends': ['website_sale'],
     'data': [
     ],
